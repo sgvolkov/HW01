8ae2a24a33f5121257819a6068a16c511eb9b614
‘sgvolkov’ <svolkov@ischool.berkeley.edu>
2015-08-04 13:05:11 -0700

    adding homework chap 2 ex 1

--- HW01_ex02_01.py
+++ HW01_ex02_01.py
@@ -3,7 +3,7 @@
 # 5
 # x = 5
 # x + 1
-# 
+#
 # (2) Now put the same statements into a script [this script] and run it. 
 ######## enter statements below this line ########
 
@@ -19,8 +19,17 @@ print("Hello World!") # example
 ######## enter statements above this line ########
 # What is the output?
 # [copy and past output here]
-# 
+# 6
 # (3) Modify the script by transforming each expression into a print statement 
 # and then run it again.
 # What is the output?
 # [copy and past output here]
+>>> print 5
+5
+>>> print x=5
+  File "<stdin>", line 1
+    print x=5
+           ^
+SyntaxError: invalid syntax
+>>> print x+1
+6
